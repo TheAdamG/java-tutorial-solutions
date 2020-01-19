8dcac95bbf840989aa10d4b65528bf7d70437571
amg2118 <amg2118@ic.ac.uk>
2020-01-19 13:55:51 +0000

    Completed Random Numbers

---
+++ Tutorials/RandomNumbers/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class Main {
+
+  public static void main(String[] args) {
+    args = new String[]{"4"};
+    Set<Integer> numberSet = new HashSet<>();
+    List<Integer> numberList = new ArrayList<>();
+    int numIn = Integer.parseInt(args[0]);
+    Random generator = new Random();
+    while (numberSet.size() < numIn) {
+      int value = generator.nextInt(numIn);
+      numberSet.add(value);
+      numberList.add(value);
+    }
+
+    System.out.println("Generating random numbers:");
+    System.out.println(numberList);
+    System.out.println("I had to generate " + numberList.size() + " numbers "
+        + "between 0 and " + (numIn - 1) + " to have produced all such "
+        + "numbers at least once.");
+  }
+
+}
