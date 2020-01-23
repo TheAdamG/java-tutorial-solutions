e7466f04e16cf4c0391cb33263e42c0901f6dcd7
amg2118 <amg2118@ic.ac.uk>
2020-01-23 18:35:24 +0000

    Reorgainsed Battling Fighters

---
+++ Tutorials/BattlingFighters/src/Fighter.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class Fighter {
+
+  private String name;
+  private String type;
+  private final int skill;
+  private int stamina;
+
+  public Fighter(String name, String type, int skill, int stamina) {
+    this.name = name;
+    this.type = type;
+    this.skill = skill;
+    this.stamina = stamina;
+  }
+
+  public void takeDamage(int damage) {
+    stamina = Math.max(stamina - damage, 0);
+  }
+
+  public int calculateDamage() {
+    return 2;
+  }
+
+  public int calculateAttackScore() {
+    Random generator = new Random();
+    return skill + generator.nextInt(6) + generator.nextInt(6) + 2;
+  }
+
+  public boolean isDead() {
+    return stamina == 0;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public int getSkill() {
+    return skill;
+  }
+
+  public int getStamina() {
+    return stamina;
+  }
+
+  public String toString() {
+    return (name + " - " + type + " - " + "skill: " + skill + "; stamina: " + stamina);
+  }
+}
